0db9ea045af887d1f1eba838508dbb335e909920
wunknown2 <wunknown2@gmail.com>
2025-12-29 19:24:46 +0900

    finaly commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-
-import '/index.css';
+import "./main.js";
+import "./index.css";
 import "./App.css";
 
-
 function App() {
- 
+  const card = [
+    {
+      id: 1,
+      title: "Salon",
+      description:
+        "Многофункциональный трёхспицевый руль с кожаной обшивкой (обычно Nappa-кожа).",
+      image:
+        "https://kolesa-uploads.ru/r/880x/e650d50b-0036-4b15-b16c-76957cb3b9e0/mercedes-e-3.jpg",
+    },
+    {
+      id: 2,
+      title: "Kapot",
+      description: "2.0 л бензиновый рядный 4-цилиндровый M254, около 204 л.с.",
+      image:
+        "https://avatars.mds.yandex.net/get-autoru-vos/3933340/015cd860ee76a84e479a626e154ac398/456x342",
+    },
+    {
+      id: 3,
+      title: "Bagazhnik",
+      description: "Объём багажника — примерно 540 литров.",
+      image:
+        "https://homato.ru/upload/resizer2/22/4fa/4fabc9035a01257f48dd4f661b7f8069.jpg",
+    },
+    {
+      id: 4,
+      title: "Kolesa",
+      description:
+        "Штатные колёса Mercedes-Benz E-Class W214: диски 17″, 18″, 19″, 20″ (реже 21″) с шинами 225/60 R17, 225/55 R18, 245/45 R19 и 275/40 R19, а также 245/40 R20 и 275/35 R20.",
+      image:
+        "https://avatars.mds.yandex.net/get-autoru-vos/1936112/54c3c930ddff92f3786e53793a221293/456x342",
+    },
+  ];
 
   return (
-    <>
-      <div className='car'>
-       <h1>Mersedes bend e calass w214</h1>
+    <div className="car">
+      <h1>Mercedes-Benz E-Class</h1>
+
+      <div className="cards">
+        {card.map((item) => (
+          <div className="card" key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        ))}
       </div>
-     
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 const card = [
   {
     id: 1,
-    title: "Card 1",
-    description: "This is the first card."
+    title: "salon",
+    description:
+      "Многофункциональный трёхспицевый руль с кожаной обшивкой (обычно Nappa-кожа) — приятный на ощупь и аккуратно оформленный.",
+    Image:
+      "https://kolesa-uploads.ru/r/880x/e650d50b-0036-4b15-b16c-76957cb3b9e0/mercedes-e-3.jpg",
   },
   {
     id: 2,
-    title: "Card 2",
-    description: "This is the second card."
+    title: "Kapot",
+    description:
+      "2.0 л бензиновый рядный 4-цилиндровый M254 — турбированный двигатель с мягкой гибридной системой (mild hybrid), мощность около 204 л. с.. Он обеспечивает хорошую эффективность, современный впрыск и улучшенные выбросы..",
+    Image:
+      "https://avatars.mds.yandex.net/get-autoru-vos/3933340/015cd860ee76a84e479a626e154ac398/456x342",
   },
   {
     id: 3,
     title: "Card 3",
-    description: "This is the third card."
-  }
-]
+    description:
+      "Стандартный объём багажника седана — примерно 540 литров у большинства бензиновых и дизельных версий",
+    Image:
+      "https://homato.ru/upload/resizer2/22/4fa/4fabc9035a01257f48dd4f661b7f8069.jpg",
+  },
+];
