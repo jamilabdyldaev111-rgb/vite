effabea448eaf4200d4420bbb4dce40c46bd25e9
wunknown2 <wunknown2@gmail.com>
2025-12-29 19:52:01 +0900

    go commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import '/index.css';
+import "./App.css";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='car'>
+       <h1>Mersedes bend e calass w214</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
